867911ab280ae2c6ef8291c707f8af8b691c5e10
Aniranth <aniranth@gmail.com>
2020-01-25 23:47:18 -0800

    Wall build finished untested

--- src/qualsbot/LandscaperBot.java
+++ src/qualsbot/LandscaperBot.java
@@ -4,6 +4,19 @@ import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
 
 public class LandscaperBot extends GameRobot {
+	private MapLocation hqLoc = null;
+	private MapLocation digLoc = null;
+	
+	private boolean in_place = false;
+	private boolean can_you_dig_it = true;
+	
+	private static int[][] offsets = {
+        new int[]{2,0},
+        new int[]{-2,0},
+		new int[]{0,2},
+		new int[]{0,-2},
+	};
+	
     public LandscaperBot(RobotController rc) throws GameActionException {
         super(rc);
     }
@@ -15,6 +28,33 @@ public class LandscaperBot extends GameRobot {
 
     @Override
     public void loop(int turn) throws GameActionException {
-        path.move(path.randomDir()); // TODO
-    }
+        if(hqLoc == null){
+            hqLoc = radio.getHQLoc();
+        }
+		if(in_place && hqLoc != null) {
+			for(int[] offset : offsets) {
+				if(rc.getLocation.isAdjacentTo(hqLoc.add(new Direction(offset[0], offset[1])))) {
+					digLoc = hqLoc.add(new Direction(offset[0], offset[1]))
+				}
+			}
+		}
+		
+		if (rc.canDigDirt(rc.getLocation().directionTo(hqLoc))) {
+			rc.digDirt(rc.getLocation().directionTo(hqLoc));
+		}
+		
+		if(in_place) {
+			if(can_you_dig_it) {
+				if(rc.canDigDirt(digLoc) {
+					rc.digDirt(digLoc);
+				}
+			} else {
+				if(rc.canDepositDirt(Direction.CENTER)) {
+					rc.depositDirt(Direction.CENTER)
+				}
+			}
+		}
+		
+		can_you_dig_it = !(getDirtCarrying() >= 1);
+	}
 }
