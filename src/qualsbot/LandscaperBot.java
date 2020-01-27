bfe8f13a401b5db8870b11eb1797654bf4e8dec6
ArclingML <parthpatel6400@gmail.com>
2020-01-26 18:02:11 -0800

    Pathfinding """works"""

--- src/qualsbot/LandscaperBot.java
+++ src/qualsbot/LandscaperBot.java
@@ -10,20 +10,25 @@ public class LandscaperBot extends GameRobot {
     private MapLocation hqLoc = null;
     private MapLocation digLoc = null;
 
-    private boolean in_place = false;
-    private boolean can_you_dig_it = true;
+    private boolean inPlace = false;
+    private boolean amOuter = false;
+    private boolean canYouDigIt = true;
 
-    private static ArrayList<MapLocation> wallLocs = new ArrayList<>();
     private static ArrayList<MapLocation> digLocs = new ArrayList<>();
-
     private static final int[][] DIG_OFFSETS = {
             new int[]{2, 0},
             new int[]{-2, 0},
             new int[]{0, 2},
             new int[]{0, -2},
+            // we only get here if we're on the corners
+            new int[]{3,3},
+            new int[]{3,-3},
+            new int[]{-3,3},
+            new int[]{-3,-3}
     };
 
-    private static final int[][] POSITION_OFFSETS = {
+    private static ArrayList<MapLocation> wallLocs = new ArrayList<>();
+    private static final int[][] INNER_WALL_OFFSETS = {
             new int[]{-1, 1},
             new int[]{0, 1},
             new int[]{1, 1},
@@ -34,6 +39,23 @@ public class LandscaperBot extends GameRobot {
             new int[]{-1, -1},
     };
 
+    private static final int OUTER_WALL_HEIGHT = 50; // desired height of outer wall before working on inner wall
+    private static ArrayList<MapLocation> supportLocs = new ArrayList<>();
+    private static final int[][] OUTER_WALL_OFFSETS = {
+            new int[]{1,2},
+            new int[]{2,2},
+            new int[]{2,1},
+            new int[]{2,-1},
+            new int[]{2,-2},
+            new int[]{1,-2},
+            new int[]{-1,-2},
+            new int[]{-2,-2},
+            new int[]{-2,-1},
+            new int[]{-2,1},
+            new int[]{-2,2},
+            new int[]{-1,2}
+    };
+
     public LandscaperBot(RobotController rc) throws GameActionException {
         super(rc);
     }
@@ -41,7 +63,7 @@ public class LandscaperBot extends GameRobot {
     @Override
     protected void init(int turn) throws GameActionException {
         if ((hqLoc = radio.getHQLoc()) != null) {
-            wallLocs = path.offsetsToLocations(POSITION_OFFSETS, hqLoc);
+            wallLocs = path.offsetsToLocations(INNER_WALL_OFFSETS, hqLoc);
             digLocs = path.offsetsToLocations(DIG_OFFSETS, hqLoc);
         }
     }
@@ -54,30 +76,52 @@ public class LandscaperBot extends GameRobot {
         }
 
 
-        if (!in_place) {
-            in_place = path.assimilate(wallLocs, hqLoc);
+        if (!inPlace) {
+            inPlace = path.assimilate(amOuter? supportLocs : wallLocs, hqLoc);
+            if(wallLocs.size() < 1){
+                wallLocs = path.offsetsToLocations(INNER_WALL_OFFSETS, hqLoc);
+                supportLocs = path.offsetsToLocations(OUTER_WALL_OFFSETS, hqLoc);
+                amOuter = true;
+            }
         } else {
             // choose a spot to dig
             if(digLoc == null) {
                 for (MapLocation l : digLocs) {
                     if (rc.getLocation().isAdjacentTo(l)) {
                         digLoc = l;
+                        break;
                     }
                 }
             }
 
             // un-drown our HQ
-            if (rc.canDigDirt(rc.getLocation().directionTo(hqLoc))) {
-                rc.digDirt(rc.getLocation().directionTo(hqLoc));
-            }
+            dig(hqLoc);
 
-            if (can_you_dig_it && digLoc != null) {
+            if (canYouDigIt && digLoc != null) {
+                System.out.println("I am digging from " + digLoc);
                 dig(digLoc);
             } else {
-                dump(Direction.CENTER);
+                if(!amOuter || rc.senseElevation(rc.getLocation()) < OUTER_WALL_HEIGHT) {
+                    dump(Direction.CENTER);
+                } else {
+                    // find the lowest point on the outer wall, then dump our dirt there
+                    MapLocation dumpSpot = null;
+                    for(int i = 0; i < wallLocs.size(); i++){
+                        MapLocation candidate = wallLocs.get(i);
+                        if(!rc.canDepositDirt(rc.getLocation().directionTo(candidate))) {
+                            // we only have results in here the first time we iterate through wallLocs
+                            wallLocs.remove(i--);
+                        } else {
+                            if(dumpSpot == null || rc.senseElevation(dumpSpot) > rc.senseElevation(candidate)){
+                                dumpSpot = candidate;
+                            }
+                        }
+                    }
+                    dump(dumpSpot);
+                }
             }
 
-            can_you_dig_it = !(rc.getDirtCarrying() >= 1);
+            canYouDigIt = !(rc.getDirtCarrying() >= 1);
         }
     }
 }
